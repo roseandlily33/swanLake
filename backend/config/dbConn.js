a084288134ef94dcde2ccb8fa7d24bb8de257966
Victoria Benoit <vrose834@gmail.com>
2023-11-18 12:48:19 -0400

    Created the note schema

---
+++ backend/config/dbConn.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const connectDb = async() => {
+    try{
+        await mongoose.connect(process.env.DATABASE_URI)
+
+    } catch(err){
+        console.log('Connection Error', err);
+    }
+};
+
+module.exports = connectDb;
--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 4200;
-const {logger} = require('./middleware/logger');
+const {logger, logEvents} = require('./middleware/logger');
 const errorHandler = require('./middleware/errorHandler');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const corsOptions = require('./config/corsOptions');
+const connectDb = require('./config/dbConn');
+const mongoose = require('mongoose');
 
+connectDb();
 
 app.use(logger);
 app.use(cors(corsOptions));
@@ -32,6 +35,17 @@ app.all('*', (req, res) => {
 })
 
 app.use(errorHandler);
-app.listen(PORT, () => {
-    console.log('Listening on port', PORT)
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to mongoDB');
+    app.listen(PORT, () => {
+        console.log('Listening on port', PORT)
+    });
 });
+
+mongoose.connection.on('error', err => {
+    console.log('Connection Error main page');
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 
+    'mongoErrorLog.log');
+})
+
