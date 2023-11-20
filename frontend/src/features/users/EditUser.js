d964c8891ff72d0f759f5d3bb7ee5f7e656e40ea
Victoria Benoit <vrose834@gmail.com>
2023-11-19 20:32:43 -0400

    Added the edit users form

--- frontend/src/features/users/EditUser.js
+++ frontend/src/features/users/EditUser.js
@@ -1,6 +1,16 @@
-
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux/es/hooks/useSelector";
+import EditUserForm from "./EditUserForm";
+import { selectUserById } from "./usersApiSlice";
 const EditUser = () => {
-    return (  );
+    const {id} = useParams();
+    const user = useSelector(state => selectUserById(state, id));
+    const content = user ? <EditUserForm user={user} /> : <p>Loading...</p>
+    return ( 
+        <>
+        {content}
+        </>
+     );
 }
  
 export default EditUser;
--- frontend/src/features/users/EditUserForm.js
+++ frontend/src/features/users/EditUserForm.js
@@ -1,24 +1,31 @@
 import { useState, useEffect } from "react";
-import { useAddNewUserMutation } from "./usersApiSlice";
+import { useUpdateUserMutation, useDeleteUserMutation } from "./usersApiSlice";
 import { useNavigate } from "react-router-dom";
-import {ROLES} from '../../config/roles';
+import { ROLES } from "../../config/roles";
 const USER_REGEX = /^[A-z]{3-20}$/;
 const PWD_REGEX = /^[A-z0-9!@#$%]{4-12}$/;
 
-const NewUserForm = () => {
-    const [addNewUser, {
+const EditUserForm = ({user}) => {
+    const [updateUser, {
         isLoading,
         isSuccess,
         isError,
         error
-    }] = useAddNewUserMutation();
+    }] = useUpdateUserMutation();
+    const [deleteUser, {
+        isSuccess: isDelSuccess,
+        isError: isDelError,
+        error: delError,
+    }] = useDeleteUserMutation();
 
     const navigate = useNavigate();
-    const [username, setUsername] = useState('');
+
+    const [username, setUsername] = useState(user.username);
     const[validUser, setValidUser] = useState(false);
     const[password, setPassword] = useState('');
     const [validPass, setValidPass] = useState(false);
-    const[roles, setRoles] = useState(["Employee"]);
+    const[roles, setRoles] = useState(user.roles);
+    const[active, setActive] = useState(user.active);
 
     useEffect(() => {
         setValidUser(USER_REGEX.test(username))
@@ -29,13 +36,14 @@ const NewUserForm = () => {
     }, [password]);
 
     useEffect(() => {
-        if(isSuccess){
+        if(isSuccess || isDelSuccess){
             setUsername('');
             setPassword('');
             setRoles([]);
             navigate('/dash/users')
         }
-    }, [isSuccess, navigate]);
+    }, [isSuccess, isDelSuccess, navigate]);
+
     const onUserChange = e => setUsername(e.target.value);
     const onPassChange = e => setPassword(e.target.password);
     const onRolesChang = e => {
@@ -45,24 +53,39 @@ const NewUserForm = () => {
         );
         setRoles(values)
     };
-    const canSave = [roles.length, validUser, validPass].every(Boolean) && !isLoading;
+    const onActiveChange = () => setActive(prev => !prev);
 
     const onSaveUser = async(e) => {
-        e.preventDefault();
-        if(canSave){
-            await addNewUser([username, password, roles])
+        //e.preventDefault();
+        if(password){
+            await updateUser({id: user.id, username, password, roles, active})
+        } else {
+            await updateUser({id: user.id, username, roles, active})
         }
     }
     const options = Object.values(ROLES).map(role => {
         return (
             <option key={role} value={role}>{role}</option>
         )
-    })
+    });
 
-    return (
+    const onDeleteUser = async() => {
+        await deleteUser({id: user.id})
+    }
+    let canSave;
+    if(password){
+     canSave = [roles.length, validUser, validPass].every(Boolean);
+    } else {
+     canSave = [roles.length, validUser, validPass].every(Boolean) && !isLoading;
+    } 
+
+    const errContent = (error?.data?.message || delError?.data?.message) ?? '';
+
+    return ( 
         <>
-        <h1>Add a new user</h1>
-        <form onSubmit={onSaveUser}>
+        {errContent}
+
+        <form onSubmit={e => e.preventDefault()}>
             <label htmlFor="username">Username</label>
             <input id="username" name="username" type="text" value="username" onChange={onUserChange} />
 
@@ -71,10 +94,16 @@ const NewUserForm = () => {
 
             <label htmlFor="roles">Roles:</label>
             <select id="roles" name="roles" multiple={true} size="3" value={roles} onChange={onRolesChang}>{options}</select>
-            <button title="save" disabled={!canSave}>Save</button>
+
+            <label htmlFor="active">Active:</label>
+            <input id="user-active" name="user-active" type="checkbox" checked={active} onChange={onActiveChange} />
+
+            <button title="save" onClick={onSaveUser}>Save</button>
+            <button title="delete" onClick={onDeleteUser}>Delete</button>
 
         </form>
-    </> );
+        </>
+     );
 }
  
-export default NewUserForm;
+export default EditUserForm;
